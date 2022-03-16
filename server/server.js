0dabc84ad88b549720d8f1012e7b41e6af072f3c
Sean <seanthewonderful@gmail.com>
2022-03-16 15:11:34 -0600

    trying app.use to display a local picture from diff folder

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "../public")))
+app.use("/images", express.static(path.join(__dirname, "../images")))
 
 const port = process.env.PORT || 4005
 
